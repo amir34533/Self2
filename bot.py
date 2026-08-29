1415708d0cf71a312373241248f769b94e2c3cca
amir34533 <amirt483923@atomicmail.io>
2026-08-29 05:22:55 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,24 +7,23 @@ from pyrogram import enums
 
 user_temp_codes = {}
 active_clients = {}
-BOT_TOKEN = "00000"
-API_ID = 00000
-API_HASH = "00000"
-ADMIN_ID = 000000
+BOT_TOKEN = "8674957786:AAHcKmPkUhR630w5pmkrifnxO9Yf8SCuVm"
+API_ID = 35656061
+API_HASH = "b37f2596516bc0439bf505d1d230395c"
+ADMIN_ID = 7845464086
 
 # تنظیمات منوی جدید
 # یوزرنیم‌ها را بدون @ وارد کنید
-SUPPORT_USERNAME = "YourSupportUsername"
-BUY_CHANNEL_USERNAME = "YourChannelUsername"
+SUPPORT_USERNAME = "Aliconfigs"
+BUY_CHANNEL_USERNAME = "SelfPersiangulf"
 HELPER_BOT_USERNAME = "Helpselfbotvippersian_bot"
 
 os.makedirs("sessions", exist_ok=True)
 
 # لیست کانال ها کم یا زیاد میتونید کنید بدون @
 FORCE_CHANNELS = [
-    "00000",
-    "00000",
-    "00000"
+    "SH0PAL1",
+ 
 ]
 
 
